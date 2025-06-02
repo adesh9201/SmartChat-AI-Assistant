f492539999445402f6b07c9006e1725f98b01012
adesh9201 <panditadesh123@gmail.com>
2025-06-02 21:00:45 +0530

    backend server updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://smartchat-ai-assistant-server.onrender.com/', // your backend server
+        target: 'https://smartchat-ai-assistant-server.onrender.com', // your backend server
         changeOrigin: true
       }
     }
